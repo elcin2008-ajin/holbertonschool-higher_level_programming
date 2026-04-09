4a6de4b827c66321ff60a907e1bee57a5e1408ce
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-04-09 08:21:01 +0000

    Fix module docstring

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module that returns attributes and methods of an object."""
 
 def lookup(obj):
+    """Return the list of available attributes and methods of an object."""
     return dir(obj)
