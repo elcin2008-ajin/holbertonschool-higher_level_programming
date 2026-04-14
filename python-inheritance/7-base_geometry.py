af545d88d4cbdba8371e647c3ff4babf00e60f1f
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-04-14 08:35:45 +0000

    Fix BaseGeometry validation

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""BaseGeometry module with validation"""
+"""BaseGeometry with validation and area method"""
 
 
 class BaseGeometry:
     """Geometry base class"""
 
     def area(self):
-        """Not implemented area method"""
+        """Not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate that value is a positive integer"""
+        """Validate integer value"""
 
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
