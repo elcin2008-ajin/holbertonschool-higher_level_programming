47b069eecb05fc148b2a7246c4eaced7cf3fcf9e
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-04-14 08:14:27 +0000

    JJ

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""This module defines a class MyList that inherits from list"""
+
 
 class MyList(list):
+    """MyList class that prints the list sorted"""
+
     def print_sorted(self):
+        """Print the list in ascending sorted order"""
         print(sorted(self))
